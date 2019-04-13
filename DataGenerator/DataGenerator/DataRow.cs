66e62f9b953a21cbb92eb20c0e7237550ff8d013
Jimmy Geels <jimmygeels@gmail.com>
2019-04-14 01:38:46 +0200

    - Data generator - ML model

---
+++ DataGenerator/DataGenerator/DataRow.cs
@@ -0,0 +1,29 @@
+﻿namespace DataGenerator
+{
+    public class DataRow
+    {
+       // HourOfDay	DayOfWeek	WeekOfYear	Month	SolarSurface	AvgGenArea	Consumed	UvIndex	GeneratedSolar
+       public int HourOfDay { get; set; }
+
+       public int DayOfWeek { get; set; }
+
+       public int WeekOfYear { get; set; }
+
+       public int Month { get; set; }
+
+       public decimal SolarSurface { get; set; }
+
+       public decimal AvgGenArea { get; set; }
+
+       public decimal Consumed { get;set; }
+
+       public decimal UvIndex { get; set; }
+
+       public decimal GeneratedSolar { get; set; }
+
+       public override string ToString()
+       {
+           return $"{HourOfDay}\t{DayOfWeek}\t{WeekOfYear}\t{Month}\t{SolarSurface.ToString("F")}\t{AvgGenArea.ToString("F")}\t{Consumed.ToString("F")}\t{UvIndex.ToString("F")}\t{GeneratedSolar.ToString("F")}";
+       }
+    }
+}
